9ff7f45329e1c80d9d51d70c3bde54b00d707f70
ImKunYoung <ur2kunyoung2@outlook.com>
2023-10-25 11:05:04 +0900

    initial commit

---
+++ src/main/java/com/example/flywaypoc/FlywayPocApplication.java
@@ -0,0 +1,13 @@
+package com.example.flywaypoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlywayPocApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FlywayPocApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/flywaypoc/FlywayPocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.flywaypoc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlywayPocApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
