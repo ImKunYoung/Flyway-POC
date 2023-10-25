cac1d20793bf3d03f80dacb2e2175e48a2951c94
ImKunYoung <ur2kunyoung2@outlook.com>
2023-10-25 11:41:31 +0900

    예시 코드 작성

--- src/main/java/com/example/flywaypoc/FlywayPocApplication.java
+++ src/main/java/com/example/flywaypoc/FlywayPocApplication.java
@@ -1,13 +1,37 @@
 package com.example.flywaypoc;
 
+import jakarta.persistence.EntityManager;
+import jakarta.transaction.Transactional;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class FlywayPocApplication {
 
+    private final EntityManager em;
+
+    public FlywayPocApplication(EntityManager em) {
+        this.em = em;
+    }
+
+
     public static void main(String[] args) {
-        SpringApplication.run(FlywayPocApplication.class, args);
+        SpringApplication app = new SpringApplication(FlywayPocApplication.class);
+        app.setWebApplicationType(WebApplicationType.NONE);
+        ConfigurableApplicationContext run = app.run(args);
+
+        FlywayPocApplication flywayPocApplication = (FlywayPocApplication)run.getBean("flywayPocApplication");
+
+        Member member = new Member("sample");
+        flywayPocApplication.save(member);
+
+    }
+
+    @Transactional
+    public void save(Member member) {
+        em.persist(member);
     }
 
 }
---
+++ src/main/java/com/example/flywaypoc/Member.java
@@ -0,0 +1,24 @@
+package com.example.flywaypoc;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Member {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+
+
+    public Member(String sample) {
+        this.name = sample;
+    }
+
+    public Member() {
+
+    }
+}
